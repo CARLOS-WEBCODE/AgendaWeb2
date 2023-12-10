7d993943a48f6711b00bd309c298f76a938cf69f
Antonio Barbosa <acrb1108@gmail.com>
2023-12-10 19:43:58 -0300

    Criando a Model - EventoCadastroViewModel

---
+++ AgendaWeb.Presentation/Models/EventoCadastoViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace AgendaWeb.Presentation.Models
+{
+    /// <summary>
+    /// Classe de modelo de dados para a página de cadastro de eventos
+    /// </summary>
+    public class EventoCadastoViewModel
+    {
+        public string? Nome { get; set; }
+        public string? Data { get; set; }
+        public string? Hora { get; set; }
+        public string? Descricao { get; set; }
+        public string? Prioridade { get; set; }
+    }
+}
