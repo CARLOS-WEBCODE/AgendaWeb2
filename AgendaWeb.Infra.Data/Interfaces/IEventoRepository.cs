800482dd5f2cf129d84765b53c978d536aace8f1
AntonioCarlosRodrigu <AntonioCarlosRodrigu@DESK32001436>
2023-12-12 09:29:02 -0300

    Criando Interfaces.

---
+++ AgendaWeb.Infra.Data/Interfaces/IEventoRepository.cs
@@ -0,0 +1,23 @@
+﻿using AgendaWeb.Infra.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaWeb.Infra.Data.Interfaces
+{
+    /// <summary>
+    /// Interface e repositorio para a entidade Evento
+    /// </summary>
+    public interface IEventoRepository : IBaseRepository<Evento>
+    {
+        /// <summary>
+        /// Método para retornar toos os eventos dentro de um período de datas
+        /// </summary>
+        /// <param name="dataMin">Data de início doperíodo</param>
+        /// <param name="dataMax">Data de término do período</param>
+        /// <returns>Lista de Eventos</returns>
+        List<Evento> GetByDatas(DateTime dataMin, DateTime dataMax);
+    }
+}
