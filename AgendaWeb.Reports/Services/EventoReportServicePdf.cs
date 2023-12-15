3e4fc8e59fefc5333c70e89972e90a37cebf0b42
Antonio Barbosa <acrb1108@gmail.com>
2023-12-14 23:05:14 -0300

    Criando evento Execel e PDF.

---
+++ AgendaWeb.Reports/Services/EventoReportServicePdf.cs
@@ -0,0 +1,28 @@
+﻿using AgendaWeb.Infra.Data.Entities;
+using AgendaWeb.Reports.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaWeb.Reports.Services
+{
+    /// <summary>
+    /// Classe para geração do relatório em formato PDF
+    /// </summary>
+    public class EventoReportServicePdf : IEventoReportService
+    {
+        /// <summary>
+        /// Método para geração do relatório
+        /// </summary>
+        /// <param name="dataMin">Data de inicio da pesquisa</param>
+        /// <param name="dataMax">Data de fim da pesquisa</param>
+        /// <param name="eventos">Lista de eventos</param>
+        /// <returns>Arquivo em formato byte</returns>
+        public byte[] Create(DateTime dataMin, DateTime dataMax, List<Evento> eventos)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
