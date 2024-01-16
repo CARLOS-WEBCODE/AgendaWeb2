c1bd7102a5ed6e7b8baf3ac8f672724782b1f965
Antonio Barbosa <acrb1108@gmail.com>
2024-01-16 20:49:19 -0300

    Criando Relacionamentos Evento - Usuario

--- AgendaWeb.Infra.Data/Entities/Usuario.cs
+++ AgendaWeb.Infra.Data/Entities/Usuario.cs
@@ -11,13 +11,19 @@ namespace AgendaWeb.Infra.Data.Entities
     /// </summary>
     public class Usuario
     {
-        #region
+        #region Propriedades
 
         public Guid Id { get; set; }
         public string? Nome { get; set; }
         public string? Email { get; set; }
         public string? Senha { get; set; }
         public DateTime DataInclusao { get; set; }
+
+        #endregion
+
+        #region Relacionamentos
+
+        public List<Evento>? Eventos { get; set; }
 
         #endregion
     }
