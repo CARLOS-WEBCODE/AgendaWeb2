4ba662e7b1060c33882b24064e62f68c85f17286
Antonio Barbosa <acrb1108@gmail.com>
2024-01-16 20:40:32 -0300

    Criando no Repository, a entidade Usuario.

---
+++ AgendaWeb.Infra.Data/Entities/Usuario.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaWeb.Infra.Data.Entities
+{
+    /// <summary>
+    /// Classe de entidade para usuario
+    /// </summary>
+    public class Usuario
+    {
+        #region
+
+        public Guid Id { get; set; }
+        public string? Nome { get; set; }
+        public string? Email { get; set; }
+        public string? Senha { get; set; }
+        public DateTime DataInclusao { get; set; }
+
+        #endregion
+    }
+}
