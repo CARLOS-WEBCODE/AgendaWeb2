86445cae76ee8120eff7f4542abc3c763dc4aee1
Antonio Barbosa <acrb1108@gmail.com>
2024-01-16 21:30:20 -0300

    Atualizando UsuarioRepository

--- AgendaWeb.Infra.Data/Repositories/UsuarioRepository.cs
+++ AgendaWeb.Infra.Data/Repositories/UsuarioRepository.cs
@@ -1,7 +1,9 @@
 ﻿using AgendaWeb.Infra.Data.Entities;
 using AgendaWeb.Infra.Data.Interfaces;
+using Dapper;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,39 +12,97 @@ namespace AgendaWeb.Infra.Data.Repositories
 {
     public class UsuarioRepository : IUsuarioRepository
     {
+        //atributo
+        private readonly string _connectionString;
+
+        //método construtor
+        public UsuarioRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+
         public void Create(Usuario obj)
         {
-            throw new NotImplementedException();
+            var query = @"
+                    INSERT INTO USUARIO(ID, NOME, EMAIL, SENHA, DATAINCLUSAO)
+                    VALUES(@Id, @Nome, @Email, @Senha, @DataInclusao)
+                ";
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Execute(query, obj);
+            }
+
         }
 
         public void Update(Usuario obj)
         {
-            throw new NotImplementedException();
+            var query = @"
+                    UPDATE USUARIO SET NOME = @Nome, 
+                    EMAIL = @Email, SENHA = @Senha
+                    WHERE ID = @Id
+                ";
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Execute(query, obj);
+            }
         }
 
         public void Delete(Usuario obj)
         {
-            throw new NotImplementedException();
+            var query = @"
+                    DELETE FROM USUARIO WHERE ID = @Id
+                ";
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Execute(query, obj);
+            }
+
         }
 
         public List<Usuario> GetAll()
         {
-            throw new NotImplementedException();
+            var query = "SELECT * FROM USUARIO";
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                return connection.Query<Usuario>(query).ToList();
+            }
+
         }
 
         public Usuario GetById(Guid id)
         {
-            throw new NotImplementedException();
+            var query = "SELECT * FROM USUARIO WHERE ID = @id";
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                return connection.Query<Usuario>(query, new { id }).FirstOrDefault();
+            }
         }
 
         public Usuario GetByEmail(string email)
         {
-            throw new NotImplementedException();
+            var query = @"SELECT * FROM USUARIO WHERE EMAIL = @email";
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                return connection.Query<Usuario>(query, new { email }).FirstOrDefault();
+            }
         }
 
         public Usuario GetByEmailESenha(string email, string senha)
         {
-            throw new NotImplementedException();
+            var query = @"SELECT * FROM USUARIO 
+                            WHERE EMAIL = @email AND SENHA = @senha";
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                return connection.Query<Usuario>(query, new { email, senha }).FirstOrDefault();
+            }
         }
     }
 }
