fbd1ebf91a3146986c4bf94a51733d1c94adaa76
Antonio Barbosa <acrb1108@gmail.com>
2024-01-16 21:09:19 -0300

    Criando UsuarioRepository

---
+++ AgendaWeb.Infra.Data/Repositories/UsuarioRepository.cs
@@ -0,0 +1,48 @@
+﻿using AgendaWeb.Infra.Data.Entities;
+using AgendaWeb.Infra.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaWeb.Infra.Data.Repositories
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        public void Create(Usuario obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Usuario obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Usuario obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Usuario> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Usuario GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Usuario GetByEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Usuario GetByEmailESenha(string email, string senha)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
