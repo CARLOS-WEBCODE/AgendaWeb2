5cd428fc48e6bc865af113a15e2bbf39c9a241e5
Antonio Barbosa <acrb1108@gmail.com>
2024-01-19 19:26:07 -0300

    Atualizando a página register.

---
+++ AgendaWeb.Infra.Data/Utils/CriptografiaUtil.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaWeb.Infra.Data.Utils
+{
+    public class CriptografiaUtil
+    {
+        public static string GetMD5(string valor)
+        {
+            var hash = new MD5CryptoServiceProvider().ComputeHash(Encoding.UTF8.GetBytes(valor));
+
+            var result = string.Empty;
+            foreach (var item in hash)
+            {
+                result += item.ToString("X2"); //Hexadecimal
+            }
+
+            return result;
+        }
+    }
+}
--- AgendaWeb.Presentation/Controllers/AccountController.cs
+++ AgendaWeb.Presentation/Controllers/AccountController.cs
@@ -1,10 +1,22 @@
-﻿using AgendaWeb.Presentation.Models;
+﻿using AgendaWeb.Infra.Data.Entities;
+using AgendaWeb.Infra.Data.Interfaces;
+using AgendaWeb.Infra.Data.Utils;
+using AgendaWeb.Presentation.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AgendaWeb.Presentation.Controllers
 {
     public class AccountController : Controller
     {
+        //atributo
+        private readonly IUsuarioRepository _usuarioRepository;
+
+        //construtor para inicialização do atributo
+        public AccountController(IUsuarioRepository usuarioRepository)
+        {
+            _usuarioRepository = usuarioRepository;
+        }
+
         public IActionResult Login()
         {
             return View();
@@ -24,6 +36,38 @@ namespace AgendaWeb.Presentation.Controllers
         [HttpPost]
         public IActionResult Register(AccountRegisterViewModel model)
         {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    //verificar se o email informado 
+                    //já está cadastrado no banco de dados
+                    if (_usuarioRepository.GetByEmail(model.Email) != null)
+                    {
+                        TempData["MensagemErro"] = $"O email informado já está cadastrado.Tente outro.";
+                    }
+                    else
+                    {
+                        var usuario = new Usuario();
+
+                        usuario.Id = Guid.NewGuid();
+                        usuario.Nome = model.Nome;
+                        usuario.Email = model.Email;
+                        usuario.Senha = CriptografiaUtil.GetMD5(model.Senha);
+                        usuario.DataInclusao = DateTime.Now;
+
+                        _usuarioRepository.Create(usuario); //cadastrando o usuário
+
+                        TempData["MensagemSucesso"] = "Usuário cadastrado com sucesso!";
+
+                        ModelState.Clear(); //limpar os campos do formulário
+                    }
+                }
+                catch (Exception e)
+                {
+                    TempData["MensagemErro"] = e.Message;
+                }
+            }
             return View();
         }
     }
